00cc1982634464f37283cf65023359dd983625b9
heysun0728 <heysun0728@gmail.com>
2018-07-25 11:07:33 +0800

    initiallize

---
+++ BLETagRegister/app/src/main/java/com/ossf/www/bletagregister/BlueToothScan.java
@@ -0,0 +1,13 @@
+package com.ossf.www.bletagregister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlueToothScan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_tooth_scan);
+    }
+}
