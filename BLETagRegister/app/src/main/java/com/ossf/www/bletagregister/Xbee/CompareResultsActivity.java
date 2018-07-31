1e8c24f78d9752b0b32ed6b17a4b57e4cdfb7e74
Tiffany <tiffany85221@gmail.com>
2018-07-31 16:30:06 +0800

    clean code

--- BLETagRegister/app/src/main/java/com/ossf/www/bletagregister/Xbee/CompareResultsActivity.java
+++ BLETagRegister/app/src/main/java/com/ossf/www/bletagregister/Xbee/CompareResultsActivity.java
@@ -16,24 +16,24 @@ import static com.ossf.www.bletagregister.HomeActivity.regDevice_list;
 
 public class CompareResultsActivity extends AppCompatActivity {
 
-    private ArrayAdapter<String> MacAddressAdapter;
-    private ListView macAddrList;
-    private ArrayList<String> mac_addresses;
+    private ArrayAdapter<String> adapter;
+    private ListView compareResultListView;
     BLEdevice device;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare_results);
 
-        macAddrList = (ListView)findViewById(R.id.mac_addresses_list);
+        compareResultListView = (ListView)findViewById(R.id.compare_result_listview);
 
-        MacAddressAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
-        macAddrList.setAdapter(MacAddressAdapter);
-        int i=0;
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        compareResultListView.setAdapter(adapter);
+        int i = 0;
         for (Map.Entry<String, BLEdevice> entry : regDevice_list.entrySet()) {
             if(listview.isItemChecked(i)) {
                 device = entry.getValue();
-                MacAddressAdapter.add("MAC: " + device.getMac() + "\nname: " + device.getName() + "\nis Found: " + device.deviceIsFound());
+                adapter.add("MAC: " + device.getMac() + "\nname: " + device.getName() + "\nis Found: " + device.deviceIsFound());
             }
             i++;
         }
